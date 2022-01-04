02b535ce52e87539689632d68f7fcf775291a1f1
louismonty <louis.mail.dk@gmail.com>
2022-01-04 14:20:46 +0100

    added: pitch to CreateEventActivity.kt fixed: PitchModel.kt
    Took 31 minutes

--- app/src/main/java/dk/bkskjold/nemsport/UI/CreateEventActivity.kt
+++ app/src/main/java/dk/bkskjold/nemsport/UI/CreateEventActivity.kt
@@ -10,6 +10,7 @@ import android.util.Log
 import android.widget.*
 import com.google.firebase.Timestamp
 import dk.bkskjold.nemsport.Helper.DatabaseHelper
+import dk.bkskjold.nemsport.Models.ClubModel
 import dk.bkskjold.nemsport.Models.EventModel
 import dk.bkskjold.nemsport.Models.PitchModel
 import dk.bkskjold.nemsport.R
@@ -18,8 +19,12 @@ import kotlin.collections.ArrayList
 
 class CreateEventActivity : AppCompatActivity() {
 
+    val now = Calendar.getInstance()
+    private var chosenDate = Date()
 
+    var thisDate = Timestamp(Calendar.getInstance().getTime())
 
+    var clubs = ArrayList<PitchModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +32,22 @@ class CreateEventActivity : AppCompatActivity() {
         val createEvent: Button = findViewById(R.id.createBtn)
         val backBtn: Button = findViewById(R.id.backBtn)
         val picktime: ImageView = findViewById(R.id.datePicker)
+        val teams = ArrayList<String>()
+        val teamsSpinner: Spinner = findViewById(R.id.spinner)
 
+        var query = DatabaseHelper.db.collection("pitches")
+
+
+        query.get().addOnSuccessListener { clubs =
+            it.toObjects(PitchModel::class.java) as ArrayList<PitchModel>
+            for (clubnames in clubs){
+                teams.add(clubnames.pitchNames)
+            }
+            val apapter: ArrayAdapter<String> = ArrayAdapter<String>(
+                this,android.R.layout.simple_spinner_item,teams
+            )
+            teamsSpinner.adapter = apapter
+        }
 
 
 
@@ -35,24 +55,28 @@ class CreateEventActivity : AppCompatActivity() {
 
         picktime.setOnClickListener{
             // https://www.youtube.com/watch?v=gollUUFBKQA&ab_channel=CodeAndroid
-            val now = Calendar.getInstance()
+
             val datePicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                Toast.makeText(this,now.toString(),Toast.LENGTH_LONG)
                 findViewById<TextView>(R.id.show_date).text = dayOfMonth.toString()+"-"+(month.toInt()+1).toString()+"-"+year.toString()
+                //chosenDate = Date(year, month,dayOfMonth, now.get(Calendar.HOUR), now.get(Calendar.MINUTE), now.get(Calendar.SECOND))
+                chosenDate.year = year
+                chosenDate.month = month
+                chosenDate.date = dayOfMonth
             }
             ,now.get(Calendar.YEAR),now.get(Calendar.MONTH),now.get(Calendar.DATE))
 
-
+            /*
             val time = Calendar.getInstance()
             val timePicker = TimePickerDialog(this,TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute -> }
             , time.get(Calendar.HOUR_OF_DAY),time.get(Calendar.MINUTE),true)
             timePicker.show()
+             */
             datePicker.show()
 
 
         }
         createEvent.setOnClickListener{
-            DatabaseHelper.createEventInDB(EventModel(findViewById<EditText>(R.id.teamNameEt).text.toString(), Timestamp(Calendar.getInstance().getTime())))
+            DatabaseHelper.createEventInDB(EventModel(findViewById<EditText>(R.id.teamNameEt).text.toString(), Timestamp(chosenDate)))
             super.onBackPressed()
         }
 
